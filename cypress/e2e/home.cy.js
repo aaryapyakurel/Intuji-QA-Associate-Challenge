ca0504825cfa5f7d4e4f022bdd33364d79acc335
aaryapyakurel <128495471+aaryapyakurel@users.noreply.github.com>
2025-08-17 23:02:25 +0545

    3
    3

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,11 +1,16 @@
-// cypress/e2e/home.cy.js
-import HomePage from '../pages/HomePage';
+describe('Automation Exercise - Home Page Test', () => {
+  it('should load the home page and navigate to Signup/Login', () => {
+    // Visit the site
+    cy.visit('https://automationexercise.com/')
 
-describe('Home Page Test', () => {
-  it('should load the homepage and go to Signup/Login', () => {
-    HomePage.visit();
-    HomePage.verifyHomePage();
-    HomePage.clickSignupLogin();
-    cy.url().should('include', '/login');
-  });
-});
+    // Verify homepage loaded
+    cy.url().should('include', 'automationexercise.com')
+    cy.contains('Home').should('be.visible')
+
+    // Click on Signup/Login
+    cy.contains('Signup / Login').click()
+
+    // Verify URL changed to /login
+    cy.url().should('include', '/login')
+  })
+})
